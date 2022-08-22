72b75e1014c13e0c407db7294260e3a099882595
lemo-o <21benjaminhall@gmail.com>
2022-08-22 12:43:22 -0600

    Add project files.

---
+++ Solutions/Classic.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FizzBuzz.Resources
+{
+    internal class Classic
+    {
+        private int _index;
+        Classic(int index)
+        {
+            _index = index;
+        }
+        public void Solution()
+        {
+            for (int i = 0; i < _index; i++)
+            {
+                if (i % 5 == 0 && i % 3 == 0)
+                    Console.WriteLine("FizzBUzz");
+                else if (i % 5 == 0)
+                    Console.WriteLine("Buzz");
+                else if (i % 3 == 0)
+                    Console.WriteLine("Fizz");
+                else
+                    Console.WriteLine(i);
+            }
+        }
+    }
+}
